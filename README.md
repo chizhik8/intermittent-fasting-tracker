dd8af9ce80416ddf4e752d2f7c942acdd54b18d5
chizhik8 <for.yu.ch@gmail.com>
2025-03-05 20:38:12 +0100

    timer functionality

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Fasting Tracker (in developing)
 
 ## Overview
-The Fasting Tracker is a simple web application designed to help users track their intermittent fasting periods.
+The Fasting Tracker is a simple web application designed to help users track their intermittent fasting periods. It provides users with a convenient interface to set and track their fasting times and automatically calculates the end time.
+
+## Key Features
+Set Fasting Start Time: Users can select the date and time to start fasting.
+Set Fasting Duration: Users can specify the duration of the fasting period in hours.
+Automatic End Time Calculation: The application automatically calculates and displays the end time of the fasting period.
+Timer: Displays the remaining time until the fasting period ends.
+Control Buttons: Buttons to start, stop, and reset the timer.
--- js/timer.js
+++ js/timer.js
@@ -1,40 +1,70 @@
 const startTimeStorage = parseInt(localStorage.getItem('startTime')) || 0;
 const elapsedTimeStorage = parseInt(localStorage.getItem('elapsedTime')) || 0;
+const durationStorage = parseInt(localStorage.getItem('duration')) || 12;
+const timeLeftsStorage = parseInt(localStorage.getItem('timeLeft')) || 0;
 
 const timerDisplay = document.getElementById('timer');
+const timeLefts = document.getElementById('time-left');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const resetBtn = document.getElementById('resetBtn');
+const duration = document.getElementById('duration');
+const startDateInput = document.getElementById('start-date');
+const endDateInput = document.getElementById('end-date');
 
 let timerInterval;
 
 const startFasting = () => {
     if (!startTimeStorage) {
-        localStorage.setItem('startTime', Date.now());
+        const startDate = new Date(startDateInput.value);
+        localStorage.setItem('startTime', startDate.getTime());
     }
+    localStorage.setItem('duration', duration.value);
     startBtn.disabled = true;
+    duration.disabled = true;
+    startDateInput.disabled = true;
     updateTimer();
     timerInterval = setInterval(updateTimer, 1000);
 };
 
 const stopFasting = () => {
-    localStorage.setItem("elapsedTime", Date.now() - parseInt(localStorage.getItem('startTime')));
-    clearInterval(timerInterval);
-    localStorage.removeItem('startTime');
-    stopBtn.disabled = true;
+    const startTime = parseInt(localStorage.getItem('startTime'));
+    if (startTime) {
+        localStorage.setItem("elapsedTime", Date.now() - startTime);
+        localStorage.setItem("timeLeft", addHoursToDate(startTime) - Date.now());
+        clearInterval(timerInterval);
+        localStorage.removeItem('startTime');
+        stopBtn.disabled = true;
+    }
 };
 
 const resetFasting = () => {
     clearInterval(timerInterval);
     localStorage.clear();
     displayTime(0);
+    displayTimeLeft(0);
     startBtn.disabled = false;
+    duration.disabled = false;
     stopBtn.disabled = false;
+    startDateInput.disabled = false;
+    startDateInput.value = formatDateTime(Date.now());
+    duration.value = 12;
 };
 
 const updateTimer = () => {
-    displayTime(Date.now() - parseInt(localStorage.getItem('startTime')));
+    const startTime = parseInt(localStorage.getItem('startTime'));
+    if (startTime) {
+        const elapsedTime = Date.now() - startTime;
+        const timeLeftMs = addHoursToDate(startTime) - Date.now();
+        displayTime(elapsedTime);
+        displayTimeLeft(timeLeftMs);
+    }
 };
+
+const setStartDateOrDuration = () => {
+    const startDate = new Date(startDateInput.value);
+    endDateInput.value = formatDateTime(addHoursToDate(startDate.getTime()));
+}
 
 const displayTime = (ms) => {
     const hours = Math.floor(ms / (1000 * 60 * 60));
@@ -43,18 +73,57 @@ const displayTime = (ms) => {
     timerDisplay.textContent = `${padNumber(hours)}:${padNumber(minutes)}:${padNumber(seconds)}`;
 };
 
+const displayTimeLeft = (ms) => {
+    const hours = Math.floor(ms / (1000 * 60 * 60));
+    const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+    timeLefts.textContent = `${padNumber(hours)}:${padNumber(minutes)}`;
+};
+
 const padNumber = (number) => number.toString().padStart(2, '0');
+
+const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+};
+
+const addHoursToDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const hours = parseInt(duration.value);
+    date.setHours(date.getHours() + hours);
+    return date;
+};
 
 startBtn.addEventListener('click', startFasting);
 stopBtn.addEventListener('click', stopFasting);
 resetBtn.addEventListener('click', resetFasting);
+startDateInput.addEventListener('change', setStartDateOrDuration);
+duration.addEventListener('change', setStartDateOrDuration);
+
+if (durationStorage) { 
+    duration.value = durationStorage;
+}
 
 if (elapsedTimeStorage) {
     displayTime(elapsedTimeStorage);
+    displayTimeLeft(timeLeftsStorage);
     startBtn.disabled = true;
     stopBtn.disabled = true;
+    duration.disabled = true;
+    startDateInput.disabled = true;
 }
 
-if (startTimeStorage) {
+if (!startTimeStorage) {
+    startDateInput.value = formatDateTime(Date.now());
+    endDateInput.value = formatDateTime(addHoursToDate(Date.now()));
+} else {
+    startDateInput.value = formatDateTime(startTimeStorage);
+    endDateInput.value = formatDateTime(addHoursToDate(startTimeStorage));
     startFasting();
 }
