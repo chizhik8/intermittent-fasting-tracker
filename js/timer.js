7be09f4ded9f704203e33067a8bcc84cde6996a8
chizhik8 <64300649+chizhik8@users.noreply.github.com>
2025-03-07 14:47:42 +0000

    timer functionality

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,11 @@
 const startTimeStorage = parseInt(localStorage.getItem('startTime')) || 0;
 const elapsedTimeStorage = parseInt(localStorage.getItem('elapsedTime')) || 0;
-const durationStorage = parseInt(localStorage.getItem('duration')) || 12;
+const durationStorage = parseInt(localStorage.getItem('duration')) || 1;
 const timeLeftsStorage = parseInt(localStorage.getItem('timeLeft')) || 0;
 
 const timerDisplay = document.getElementById('timer');
 const timeLefts = document.getElementById('time-left');
+const timeLeftsMessage = document.getElementById('time-left-message');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const resetBtn = document.getElementById('resetBtn');
@@ -15,8 +16,8 @@ const endDateInput = document.getElementById('end-date');
 let timerInterval;
 
 const startFasting = () => {
+    const startDate = new Date(startDateInput.value);
     if (!startTimeStorage) {
-        const startDate = new Date(startDateInput.value);
         localStorage.setItem('startTime', startDate.getTime());
     }
     localStorage.setItem('duration', duration.value);
@@ -53,18 +54,27 @@ const resetFasting = () => {
 
 const updateTimer = () => {
     const startTime = parseInt(localStorage.getItem('startTime'));
-    if (startTime) {
+    if (startTime <= Date.now()) {
         const elapsedTime = Date.now() - startTime;
         const timeLeftMs = addHoursToDate(startTime) - Date.now();
         displayTime(elapsedTime);
         displayTimeLeft(timeLeftMs);
+
+        if (timeLeftMs < 0) {
+            timeLeftsMessage.textContent = 'above';
+        }
     }
 };
 
 const setStartDateOrDuration = () => {
     const startDate = new Date(startDateInput.value);
+    if (parseInt(duration.value) < 0) {
+        duration.value = 0;
+    }
     endDateInput.value = formatDateTime(addHoursToDate(startDate.getTime()));
 }
+
+const padNumber = (number) => number.toString().padStart(2, '0');
 
 const displayTime = (ms) => {
     const hours = Math.floor(ms / (1000 * 60 * 60));
@@ -74,13 +84,10 @@ const displayTime = (ms) => {
 };
 
 const displayTimeLeft = (ms) => {
-    const hours = Math.floor(ms / (1000 * 60 * 60));
-    const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+    const hours = Math.floor(ms / (1000 * 60 * 60)) < 0 ? Math.floor(ms / (1000 * 60 * 60))*-1 : Math.floor(ms / (1000 * 60 * 60));
+    const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60)) < 0 ? Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60))*-1 : Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
     timeLefts.textContent = `${padNumber(hours)}:${padNumber(minutes)}`;
 };
-
-const padNumber = (number) => number.toString().padStart(2, '0');
 
 const formatDateTime = (timestamp) => {
     const date = new Date(timestamp);
@@ -95,7 +102,7 @@ const formatDateTime = (timestamp) => {
 
 const addHoursToDate = (timestamp) => {
     const date = new Date(timestamp);
-    const hours = parseInt(duration.value);
+    const hours = parseInt(duration.value) < 0 ? 0 : parseInt(duration.value);
     date.setHours(date.getHours() + hours);
     return date;
 };
